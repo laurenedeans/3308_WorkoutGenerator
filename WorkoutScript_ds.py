148600a96c01dea1325c9a794e47b2ebd3148a26
Derek Sessions <Derek.sessions@colorado.edu>
2019-03-07 17:04:20 -0700

    Made changes to Lauren's WorkoutScript.py Should be easier to edit in the future

---
+++ WorkoutScript_ds.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import os
+import sys
+import csv
+import random
+
+Arms = []
+Legs = []
+Back = []
+Abdominals = []
+Cardio = []
+Chest = []
+
+# Uses one function to create workouts of different lengths
+def make_workout(arm_no, leg_no, back_no, chest_no, abs_no):
+	Workout = []
+	for i in range(arm_no):
+		Workout.append(random.choice(Arms))
+	for i in range(leg_no):
+		Workout.append(random.choice(Legs))
+	for i in range(back_no):
+		Workout.append(random.choice(Back))
+	for i in range(chest_no):
+		Workout.append(random.choice(Chest))
+	for i in range(abs_no):
+		Workout.append(random.choice(Abdominals))
+	print("\nHere is your", workolen, "workout:\n")
+	
+	print("Arm Exercises ("+str(arm_no)+" randomly chosen):")
+	for i in range(arm_no):
+		print("\t",Workout[i][0], "--", Workout[i][1], "("+Workout[i][2]+")", "--", Workout[i][3]+",", Workout[i][4])
+	
+	print("\nLeg Exercises ("+str(leg_no)+" randomly chosen):")
+	for i in range(leg_no):
+		print("\t",Workout[i+arm_no][0], "--", Workout[i+arm_no][1], "("+Workout[i+arm_no][2]+")", "--", Workout[i+arm_no][3]+",", Workout[i+arm_no][4])
+	
+	print("\nBack Exercises ("+str(back_no)+" randomly chosen):")
+	for i in range(back_no):
+		print("\t",Workout[i+arm_no+leg_no][0], "--", Workout[i+arm_no+leg_no][1], "("+Workout[i+arm_no+leg_no][2]+")", "--", Workout[i+arm_no+leg_no][3]+",", Workout[i+arm_no+leg_no][4])
+	
+	print("\nChest Exercises ("+str(chest_no)+" randomly chosen):")
+	for i in range(chest_no):
+		print("\t",Workout[i+arm_no+leg_no+back_no][0], "--", Workout[i+arm_no+leg_no+back_no][1], "("+Workout[i+arm_no+leg_no+back_no][2]+")", "--", Workout[i+arm_no+leg_no+back_no][3]+",", Workout[i+arm_no+leg_no+back_no][4])
+
+	print("\nAbdominal Exercises ("+str(abs_no)+" randomly chosen):")
+	for i in range(abs_no):
+		print("\t",Workout[i+arm_no+leg_no+back_no+chest_no][0], "--", Workout[i+arm_no+leg_no+back_no+chest_no][1], "("+Workout[i+arm_no+leg_no+back_no+chest_no][2]+")", "--", Workout[i+arm_no+leg_no+back_no+chest_no][3]+",", Workout[i+arm_no+leg_no+back_no+chest_no][4])
+
+	print("\n")
+
+workolen = sys.argv[1]
+f = open('Exercises.csv')
+csv_f = csv.reader(f)
+
+# group exercises by muscle group
+for row in csv_f:
+	if row[1]=='Arms': Arms.append(row)
+	elif row[1]=='Legs': Legs.append(row)
+	elif row[1]=='Back': Back.append(row)
+	elif row[1]=='Abdominals': Abdominals.append(row)
+	elif row[1]=='Cardio': Cardio.append(row)
+	elif row[1]=='Chest': Chest.append(row)
+
+if workolen == "short":
+	make_workout(2,2,1,1,0)
+elif workolen == "medium":
+	make_workout(2,2,1,1,3)
+elif workolen == 'long':
+	make_workout(2,3,3,1,3)
