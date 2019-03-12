54ee1e89de0c97e97fce702a48105aa3c37acc9f
brmu4245 <37051627+brmu4245@users.noreply.github.com>
2019-03-11 23:01:50 -0600

    Brets work (#4)
    * Add files via upload
    
    Adding list of possible databases
    
    * Changes to WorkoutScript_ds.py and script_test.py
    
    * Add files via upload
    
    updated script_test.py and WorkoutScript_ds.py
    
    * Formatted main script...prints to a txt file
    
    * Update WorkoutScript_ds.py, 3 tests script_test.py
    
    * 3-10-19 (Sunday) Main reason: TESTING.md
    reason: TESTING.md update3-10-19 (Sunday) Main reason: TESTING.md update
    
    * Fixed the extra intro line and tests to match.
    
    * Removing workout_print.txt from revision control.
    
    * Should not be under revision control.
    
    * Addressing some PR comments.

--- TESTING.md
+++ TESTING.md
@@ -1,4 +1,13 @@
-# This is to create test cases for your current script
+Project Title:
+
+Team: Lauren Deans, Derek Sessions, Christina Holt, Bret Murray
+
+Automated Test Cases: To run the automated test cases, type the following onto the command line:
+
+				./script_test.py
+
+User Acceptance Testing:
+# This is to create test cases for the user acceptance testing:
 
 # Use case name
 
--- WorkoutScript_ds.py
+++ WorkoutScript_ds.py
@@ -3,74 +3,66 @@ import os
 import sys
 import csv
 import random
+from collections import OrderedDict
 
-Arms = []
-Legs = []
-Back = []
-Abdominals = []
-Cardio = []
-Chest = []
+muscle_group = { 
+    'Arms': [],
+    'Legs': [],
+    'Back': [],
+    'Abdominals': [],
+    'Cardio': [],
+    'Chest': [],
+    }
 
+with open('Exercises.csv', 'r') as f:
+    csv_f = csv.reader(f)
+
+    # group exercises by muscle group
+    for row in csv_f:
+        muscle_group.get(row[1], []).append(row)
+
+# main function catches any errors in calling on program.
 def main():
     if len(sys.argv)==1:
-        print("\nUsage: ./WorkoutScript_ds.py WORKOUT_LENGTH TARGET_AREA")
+        print("\nUsage: ./WorkoutScript_ds.py WORKOUT_LENGTH")
         print(" -- WORKOUT_LENGTH = short (30min), medium (45min), long (60min)\n")
     else:
         length = sys.argv[1]
         start_wkout(length)
 
+# This is part of the program that captures the person's choices.
+# to add focus area, create a separate function.
 def start_wkout(length_choice):
     WORKOUT_LENGTH = length_choice
-    f = open('Exercises.csv')
-    csv_f = csv.reader(f)
-
-    # group exercises by muscle group
-    for row in csv_f:
-        if row[1]=='Arms': Arms.append(row)
-        elif row[1]=='Legs': Legs.append(row)
-        elif row[1]=='Back': Back.append(row)
-        elif row[1]=='Abdominals': Abdominals.append(row)
-        elif row[1]=='Cardio': Cardio.append(row)
-        elif row[1]=='Chest': Chest.append(row)
-    print("\nHere is your", WORKOUT_LENGTH, "workout:\n")
-    if WORKOUT_LENGTH == "short": make_workout(2,2,1,1,0)
-    elif WORKOUT_LENGTH == "medium": make_workout(2,2,1,1,3)
-    elif WORKOUT_LENGTH == 'long': make_workout(2,3,3,1,3)
+    if WORKOUT_LENGTH == "short": predefined_set = (2,2,1,1,0)
+    elif WORKOUT_LENGTH == "medium": predefined_set = (2,3,1,1,2)
+    elif WORKOUT_LENGTH == 'long': predefined_set = (3,2,2,3,2)
+    return make_workout(*predefined_set)
 
 # Uses one function to create workouts of different lengths
 def make_workout(arm_no, leg_no, back_no, chest_no, abs_no):
-	Workout = []
-	for i in range(arm_no):
-		Workout.append(random.choice(Arms))
-	for i in range(leg_no):
-		Workout.append(random.choice(Legs))
-	for i in range(back_no):
-		Workout.append(random.choice(Back))
-	for i in range(chest_no):
-		Workout.append(random.choice(Chest))
-	for i in range(abs_no):
-		Workout.append(random.choice(Abdominals))
-	
-	print("Arm Exercises ("+str(arm_no)+" randomly chosen):")
-	for i in range(arm_no):
-		print("\t",Workout[i][0], "--", Workout[i][1], "("+Workout[i][2]+")", "--", Workout[i][3]+",", Workout[i][4])
-	
-	print("\nLeg Exercises ("+str(leg_no)+" randomly chosen):")
-	for i in range(leg_no):
-		print("\t",Workout[i+arm_no][0], "--", Workout[i+arm_no][1], "("+Workout[i+arm_no][2]+")", "--", Workout[i+arm_no][3]+",", Workout[i+arm_no][4])
-	
-	print("\nBack Exercises ("+str(back_no)+" randomly chosen):")
-	for i in range(back_no):
-		print("\t",Workout[i+arm_no+leg_no][0], "--", Workout[i+arm_no+leg_no][1], "("+Workout[i+arm_no+leg_no][2]+")", "--", Workout[i+arm_no+leg_no][3]+",", Workout[i+arm_no+leg_no][4])
-	
-	print("\nChest Exercises ("+str(chest_no)+" randomly chosen):")
-	for i in range(chest_no):
-		print("\t",Workout[i+arm_no+leg_no+back_no][0], "--", Workout[i+arm_no+leg_no+back_no][1], "("+Workout[i+arm_no+leg_no+back_no][2]+")", "--", Workout[i+arm_no+leg_no+back_no][3]+",", Workout[i+arm_no+leg_no+back_no][4])
+    Workout = []
+    focus_num = OrderedDict({
+        'Arms': arm_no,
+        'Legs': leg_no,
+        'Back': back_no,
+        'Abdominals': chest_no,
+        'Chest': abs_no,
+        })
 
-	print("\nAbdominal Exercises ("+str(abs_no)+" randomly chosen):")
-	for i in range(abs_no):
-		print("\t",Workout[i+arm_no+leg_no+back_no+chest_no][0], "--", Workout[i+arm_no+leg_no+back_no+chest_no][1], "("+Workout[i+arm_no+leg_no+back_no+chest_no][2]+")", "--", Workout[i+arm_no+leg_no+back_no+chest_no][3]+",", Workout[i+arm_no+leg_no+back_no+chest_no][4])
-	print("\n")
+    # creates a list of chosen workouts in list: Workout
+    for area, num in focus_num.items():
+        sample = random.sample(muscle_group[area], min(num, len(muscle_group[area])))
+        Workout.extend(sample)
+    
+    # prints (or sends to another txt file) the proper, formatted list 
+     
+    wkout_list_idx = -1
+    for exercise in Workout:
+        print("\t" + exercise[0] + " -- " +  exercise[1] + " (" + exercise[2] + ")" + 
+                " -- " + exercise[3] + ", " + exercise[4] + "\n")
+	
+    return Workout
 
 
 
