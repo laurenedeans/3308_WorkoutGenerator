e533fd797e662614c1b077da95babb599c9663da
DRSessions <38146845+DRSessions@users.noreply.github.com>
2019-04-30 17:52:44 -0600

    Ds (#7)
    * Added to usage statement
    
    * Added filter function for equipment & experience.
     -- doesn't remove from dictionary yet, just prints filtered exercises
    
    * Implemented features for command line arguments for WORKOUT_LENGTH,
    TARGET_AREA, EXPERIENCE, GOAL, EQUIPMENT.
    
    * Pushing functional WorkoutScript_ds.py to my branch. Script not finished
    yet. Also did search and replace for "dumbell" to "dumbbell" in
    Exercises.csv
    
    * Implemented distribute_exercise() function Christina provided and made
    it fit within our existing code. Still need to properly parse
    TARGET_AREA command line input.
    
    * Converting input to argparse

--- WorkoutScript_ds.py
+++ WorkoutScript_ds.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import sys
 import csv
 import random
+import numpy as np
 from collections import OrderedDict
 
 muscle_group = { 
@@ -14,43 +16,100 @@ muscle_group = {
     'Chest': [],
     }
 
+# group exercises by muscle group
 with open('Exercises.csv', 'r') as f:
     csv_f = csv.reader(f)
-
-    # group exercises by muscle group
     for row in csv_f:
         muscle_group.get(row[1], []).append(row)
 
+# Calling w/ 1 arg: ./WorkoutScript_ds.py
 # main function catches any errors in calling on program.
 def main():
-    if len(sys.argv)==1:
-        print("\nUsage: ./WorkoutScript_ds.py WORKOUT_LENGTH")
-        print(" -- WORKOUT_LENGTH = short (30min), medium (45min), long (60min)\n")
-    else:
-        length = sys.argv[1]
-        start_wkout(length)
+    areas = 'Arms,Legs,Chest,Back,Abdominals'.split(',')
+    equipment = 'Dumbells, Bar, Machine, Body Weight, Medicine Ball, Jump rope, Pool, Bicycle'
+    parser = argparse.ArgumentParser(description='Workout generator')
+    parser.add_argument('-L', '--WORKOUT_LENGTH',
+            choices=['short', 'medium', 'long'],
+            required=True,
+            help='Lenght of workout: short (30min - 6 exercises), medium (45min - 9 exercises), long (60min - 12 exercises)',
+            )
+    parser.add_argument('-E', '--EXPERIENCE',
+            choices=['Beginner', 'Intermediate', 'Advanced'],
+            required=True,
+            help='User experience level',
+            )
+    parser.add_argument('-A', '--TARGET_AREA',
+            nargs='*', # All input args are gathered into a list
+            help=f'Target areas: {areas}',
+            )
+    parser.add_argument('-G', '--GOAL',
+            choices=['Mass', 'Strength', 'Endurance'],
+            required=True,
+            help='Workout target areas',
+            )
+    parser.add_argument('-Q', '--EQUIPMENT',
+            required=True,
+            nargs='*', # All input args are gathered into a list
+            help=f'List of equipment user has access to: {equipment}',
+            )
+    args = parser.parse_args()
 
-# This is part of the program that captures the person's choices.
-# to add focus area, create a separate function.
-def start_wkout(length_choice):
-    WORKOUT_LENGTH = length_choice
-    if WORKOUT_LENGTH == "short": predefined_set = (2,2,1,1,0)
-    elif WORKOUT_LENGTH == "medium": predefined_set = (2,3,1,1,2)
-    elif WORKOUT_LENGTH == 'long': predefined_set = (3,2,2,3,2)
-    return make_workout(*predefined_set)
 
-# Uses one function to create workouts of different lengths
+    length_map = {
+        'short': 6,
+        'medium': 9,
+        'long': 12,
+        }
+    WORKOUT_LENGTH = length_map[args.WORKOUT_LENGTH]
+
+    TARGET_AREA = [1 if area in args.TARGET_AREA else 0 for area in areas]
+    EXPERIENCE = args.EXPERIENCE
+    GOAL = args.GOAL
+    EQUIPMENT = ["Body Weight"] + args.EQUIPMENT
+
+    filter_exer(muscle_group, EQUIPMENT, EXPERIENCE)  # filters existing dictionary to only contain valid exercises
+    workout_dist = distribute_exercise(WORKOUT_LENGTH, np.array(TARGET_AREA)) # returns np.array that gives number of exercises for each muscle group
+    print(workout_dist)
+    make_workout(*workout_dist)
+
+
+def distribute_exercise(len_choice, target_choice):
+
+    ret = target_choice * len_choice
+    bound = len_choice / sum(target_choice) - 1
+
+    # Put some bounds on the distribution
+    # Don't allow large numbers, large differences between min/max, or zeros where they shouldn't be
+    too_large = ret > (len_choice - bound)
+    zeroed = np.logical_xor(target_choice, ret)
+    big_diffs = (np.max(ret[ret>0]) - np.min(ret[ret>0])) > bound + 1
+    one_target_area = sum(target_choice) == 1
+
+    # Pull from a random multinomial distribution until conditions are met
+    while (not one_target_area and any(too_large)) or any(zeroed) or big_diffs:
+        ret = np.random.multinomial(len_choice, target_choice/sum(target_choice))
+        too_large = ret > (len_choice - bound)
+        zeroed = np.logical_xor(target_choice, ret)
+        big_diffs = (np.max(ret[ret>0]) - np.min(ret[ret>0])) >= bound + 1
+    return ret
+
+
+
 def make_workout(arm_no, leg_no, back_no, chest_no, abs_no):
+    #  -- Input: predefined_set (from distribute_exercise())
+    #  -- Output: List in the form [sets, reps, weight, exercise_list]
+    
     Workout = []
+
     focus_num = OrderedDict({
         'Arms': arm_no,
         'Legs': leg_no,
         'Back': back_no,
-        'Abdominals': chest_no,
-        'Chest': abs_no,
+        'Chest': chest_no,
+        'Abdominals': abs_no,
         })
 
-    # creates a list of chosen workouts in list: Workout
+    # Takes random sample from dictionary, adds new exercises to workout
     for area, num in focus_num.items():
         sample = random.sample(muscle_group[area], min(num, len(muscle_group[area])))
         Workout.extend(sample)
@@ -65,6 +124,26 @@ def make_workout(arm_no, leg_no, back_no, chest_no, abs_no):
     return Workout
 
 
+def filter_exer(MuscleGroup, EQUIPMENT, EXPERIENCE):
+    # Modifies the existing dictionary, replacing the existing
+    #print("Filtered Exercises:")
+    for area, exercise_list in MuscleGroup.items():
+        valid_exercises = []
+        for exercise in exercise_list:
+            if EXPERIENCE == "Advanced":
+                if (exercise[3] in EQUIPMENT):
+                    #print(exercise[0], "\n\t -- ", exercise[1], " -- ", exercise[3], " -- ", exercise[5])
+                    valid_exercises.append(exercise)
+            elif EXPERIENCE == "Intermediate":
+                if (exercise[3] in EQUIPMENT) & (exercise[5] != "Advanced"):
+                    #print(exercise[0], "\n\t -- ", exercise[1], " -- ",exercise[3], " -- ", exercise[5])
+                    valid_exercises.append(exercise)
+            else:
+                if (exercise[3] in EQUIPMENT) & (exercise[5] == "Beginner"):
+                    #print(exercise[0], "\n\t -- ", exercise[1], " -- ",exercise[3], " -- ", exercise[5])
+                    valid_exercises.append(exercise)
+        muscle_group[area] = valid_exercises
+    #print("\n\n")
 
 #if __name__ == "__main__":
  #   main()
